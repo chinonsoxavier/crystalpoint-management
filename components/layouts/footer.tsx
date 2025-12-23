199fc520b76622546f64873d31253fee5995ed1a
daffydanni <daffydanni@gmail.com>
2025-12-23 19:23:57 +0100

    updated

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -19,7 +19,6 @@ const Footer = () => {
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
     setIsClient(true);
   }, []);
 
--- lib/i18n/index.ts
+++ lib/i18n/index.ts
@@ -2,16 +2,16 @@ import { header as enHeader } from "./en/header";
 import { header as esHeader } from "./es/header";
 import { header as frHeader } from "./fr/header";
 import { header as deHeader } from "./de/header";
-import { header as zhHeader } from "./zh/header";
+//import { header as zhHeader } from "./zh/header";
 
-export type Language = "en" | "es" | "fr" | "de" | "zh"| "ar";
+export type Language = "en" | "es" | "fr" | "de" | "ar";
 
 export const translations = {
   en: { header: enHeader },
   es: { header: esHeader },
   fr: { header: frHeader },
   de: { header: deHeader },
-  zh: { header: zhHeader },
+  //zh: { header: zhHeader },
   ar: { header: enHeader },
 };
 
@@ -21,6 +21,6 @@ export const languages: Record<Language, string> = {
   es: "Español",
   fr: "Français",
   de: "Deutsch",
-  zh: "中文",
+ // zh: "中文",
   ar: "العربية",
 };
